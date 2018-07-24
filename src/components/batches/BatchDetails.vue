ff18193ece5a31a3b79826c791a247c49154c7d9
Sharat Chandra <sharat910@gmail.com>
2018-07-24 13:43:37 +0530

    Added monthly statement downloader.

--- src/components/batches/BatchDetails.vue
+++ src/components/batches/BatchDetails.vue
@@ -2,7 +2,42 @@
   <div class="details">
     <div class="container">
       <Alert v-if="alert.raised" :message="alert.message" :type="alert.type" @alertClosed="closeAlert()"/>
-    <router-link class="btn btn-default" :to="{name: 'Batches'}">Back</router-link>
+
+    <div class="top">
+        <router-link class="btn btn-default" :to="{name: 'Batches'}">Back</router-link>
+        <span class="pull-right">
+        <div class="form-inline">
+          <div class="form-group">
+
+            <label class="col-sm-4 control-label ">
+              Statement Month
+            </label>
+
+
+            <div class="col-sm-4">
+              <select class="form-control" name="level" v-model="statement_month">
+                      <option value="1">January</option>
+                      <option value="2">February</option>
+                      <option value="3">March</option>
+                      <option value="4">April</option>
+                      <option value="5">May</option>
+                      <option value="6">June</option>
+                      <option value="7">July</option>
+                      <option value="8">August</option>
+                      <option value="9">September</option>
+                      <option value="10">October</option>
+                      <option value="11">November</option>
+                      <option value="12">December</option>
+              </select>
+            </div>
+          </div>
+
+          <button @click="downloadStatement()" class="btn" title="Download the monthly statement">Download</button>
+      </div>
+    </span>
+
+    </div>
+
     <h1 class="page-header">{{batch.level_detail}} | {{batch.teacher_name}}
         <span class="pull-right">
           <router-link class="btn btn-primary" :to="{name:'EditBatch',props: {id:batch.id}}">Edit</router-link>
@@ -78,6 +113,7 @@
 import Students from '@/components/students/Students';
 import Alert from '@/components/Alert';
 import {restAPI} from '@/services/rest-api';
+import axios from 'axios';
 
 export default {
   name: 'batchdetails',
@@ -86,6 +122,7 @@ export default {
       batch: {
         'centre_exp': {},
       },
+      statement_month: -1,
       alert:{
         message: '',
         type: '',
@@ -109,6 +146,37 @@ export default {
           }).catch(e => {
             console.error(e);console.error(e.response)
           })
+      },
+      downloadStatement(){
+        var data = {
+          'batch': this.batch,
+          'month': parseInt(this.statement_month)
+        }
+        restAPI.post('batch/' + this.batch.id + '/generate_monthly_statement/',data)
+        .then(response => {
+          var url = response.data['url'];
+          var final_url = restAPI.defaults.baseURL.slice(0,-3) + url
+          console.log(final_url)
+          this.downloadFile(final_url)
+        }).catch(e => {
+          console.error(e);console.error(e.response)
+        })
+      },
+      downloadFile(complete_url){
+        axios({
+          url: complete_url,
+          method: 'GET',
+          responseType: 'blob', // important
+          headers: { 'Accept': 'application/vnd.ms-excel' }
+        }).then((response) => {
+          const url = window.URL.createObjectURL(new Blob([response.data]));
+          const link = document.createElement('a');
+          link.href = url;
+          var file_name = complete_url.split("/").pop()
+          link.setAttribute('download', file_name);
+          document.body.appendChild(link);
+          link.click();
+        });
       },
       raiseAlert(message,type){
         this.alert.message = message;
--- src/services/rest-api.js
+++ src/services/rest-api.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
+import http from 'http';
 
 export const restAPI = axios.create({
   baseURL: 'http://192.168.0.120:8000/api',
+  httpAgent: new http.Agent({ keepAlive: true }),
 })
